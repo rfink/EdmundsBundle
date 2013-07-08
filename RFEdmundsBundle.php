67d0421b7ebf86b564e80c213d9fe9701f643746
Ryan Fink <ryanjfink@gmail.com>
2013-07-07 23:15:41 -0400

    Initial commit

---
+++ RFEdmundsBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RF\EdmundsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Edmunds API Bundle
+ *
+ * @author Ryan Fink <ryanjfink@gmail.com>
+ */
+class RFEdmundsBundle extends Bundle
+{
+}
